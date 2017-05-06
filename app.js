fec92982babc2cfaf250a8e5646e7a243ec6c066
Bart Bucknill <bartbucknill@hotmail.com>
2017-05-06 22:18:19 +0100

    files created

---
+++ app.js
@@ -0,0 +1,3 @@
+function add(a, b) { return a + b; }
+
+function hi(name) { return `hi ${name}`; }
---
+++ test/tests.js
@@ -0,0 +1,5 @@
+// QUNIT TESTS
+
+QUnit.test('return hi plus name arg', function(assert) {
+    assert.equal(hi('jemal'), 'hi jemal', 'should return "hi jemal"');
+});
