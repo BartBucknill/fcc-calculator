c4d496d135c2df0da75f7ff6248cc72b9debe758
Bart Bucknill <bartbucknill@hotmail.com>
2017-05-11 10:53:15 +0100

    shuntModel & tests

--- app.js
+++ app.js
@@ -1,3 +1,61 @@
-function add(a, b) { return a + b; }
+'use strict';
+//output from prepDataModel to be as the below
+let data = [2,'+',2,'*',4,'-',8, '/', 2];
 
-function hi(name) { return `hi ${name}`; }
+// let prepDataModel = {
+//     removeSpaces(string) {
+//         return string.replace(/ /g, '');
+//     },
+//      string.split(/([*/+-/])/g).forEach(item => 
+//             item = parseFloat(item) || item;
+// }
+
+let shuntModel = {
+
+    queue: [],
+
+    stack: [],
+
+    run(array) {
+        this.parseArray(array);
+        this.pushStackToQueue();
+    },
+    
+    parseArray(array) {
+        array.forEach(item => {
+            if (this.isNum(item)) { this.queue.push(item); }
+            else { this.placeOperator(item); }
+        })
+    },
+
+    pushStackToQueue() {
+        while(this.stack.length > 0) {
+           this.queue.push(this.stack.pop());
+        }
+    },
+
+    placeOperator(op) {
+        while (this.topOfStackIsHigherPrecedence(op, this.topOfStack())) {
+            this.queue.push(this.stack.pop());
+        }
+        this.stack.push(op);
+    },
+
+    topOfStackIsHigherPrecedence(currentOp, stackTopOp) {
+        let reg = /[*\/]/;
+        return (reg.test(stackTopOp) && !reg.test(currentOp));
+    },
+
+    topOfStack() { return this.stack[this.stack.length - 1]; },
+
+    isNum(item) {
+        return typeof(item) === 'number';
+    }
+
+}
+
+let postfixEvalModel = {
+
+
+
+}
--- test/tests.js
+++ test/tests.js
@@ -1,5 +1,60 @@
 // QUNIT TESTS
 
-QUnit.test('return hi plus name arg', function(assert) {
-    assert.equal(hi('jemal'), 'hi jemal', 'should return "hi jemal"');
+// QUnit.test('remove spaces from string', function(assert) {
+//     assert.strictEqual(modelShunt.removeSpaces('a b c'), 'abc', '"a b c" should become "abc"');
+//     assert.strictEqual(modelShunt.removeSpaces(' 1+ 2 /8  8* 9  '), '1+2/88*9', '" 1+ 2 /8  8* 9  " should become "1+2/88*9"');
+// });
+
+
+//SHUNTMODEL TESTS 
+
+//helper: reset stack and queue after tests
+function clearStackAndQueue() { 
+    shuntModel.stack = []; 
+    shuntModel.queue = [];
+}
+
+
+QUnit.test('check if operator on top of stack is of higher precedence than current operator', function(assert) {
+    assert.strictEqual(shuntModel.topOfStackIsHigherPrecedence('-', '*'), true, '* > -, should return true');
+    assert.strictEqual(shuntModel.topOfStackIsHigherPrecedence('/', '+'), false, '+ < /, should return false');
 });
+
+QUnit.test('return operator on top of stack', function(assert) {
+    shuntModel.stack.push('+');
+    assert.strictEqual(shuntModel.topOfStack(), '+', 'should return +');
+    clearStackAndQueue(); 
+});
+
+QUnit.test('check if item is a number', function(assert) {
+   assert.strictEqual(shuntModel.isNum(10), true, '10 is a number');
+   assert.strictEqual(shuntModel.isNum(-99999), true, '-99999 is a number');
+   assert.strictEqual(shuntModel.isNum(''), false, 'empty string is not a number');
+   assert.strictEqual(shuntModel.isNum('2'), false, 'string "2" is not a number');
+});
+
+QUnit.test('if higher precedence operator than current is on stack, it should be moved to queue, when not current should be placed on stack', function(assert) {
+    shuntModel.stack.push('*');
+    shuntModel.placeOperator('+');
+    assert.deepEqual(shuntModel.queue, ['*'], '* > +, so * should be moved from stack to queue');
+    assert.deepEqual(shuntModel.stack, ['+'], '+ should have been placed on stack');
+    clearStackAndQueue();
+});
+
+QUnit.test('if multiple higher precedence operators are on stack, they should be all be popped and moved to queue, only then should current be placed on stack', function(assert) {
+    shuntModel.stack.push('+','*', '/', '*');
+    shuntModel.placeOperator('-');
+    assert.deepEqual(shuntModel.queue, ['*', '/', '*'], 'all higher precedence operators on stack should have been moved to queue');
+    assert.deepEqual(shuntModel.stack, ['+','-'], 'lower precedence operator should remain on stack and current operator should have been placed there');
+    clearStackAndQueue();
+});
+
+QUnit.test('when an infix expression as an array of operators (as strings) and numbers is passed to shuntModel.run() they should be placed in postfix in queue array', function(assert) {
+    shuntModel.run([99,'+',-999,'/',8,'+',0,'*',0.5]);
+    assert.deepEqual(shuntModel.queue, [99,-999,8,'/',0,0.5,'*','+','+'],'operators and numbers should be in queue in correct order (postfix)');
+    assert.deepEqual(shuntModel.stack, [], 'stack should be empty');
+    clearStackAndQueue();
+});
+
+//POSTFIXEVALMODEL TESTS
+
